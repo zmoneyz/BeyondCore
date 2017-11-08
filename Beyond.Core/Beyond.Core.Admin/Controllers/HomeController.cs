e5066c1f81cb852177d2de574a068d5f35babd7b
Allen Lee <lx_sqrz@126.com>
2017-11-08 18:00:24 +0800

    添加logo图标及wibconfig

--- Beyond.Core/Beyond.Core.Admin/Controllers/HomeController.cs
+++ Beyond.Core/Beyond.Core.Admin/Controllers/HomeController.cs
@@ -11,12 +11,14 @@ namespace Beyond.Core.Admin.Controllers
     public class HomeController : Controller
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-        public ActionResult Index(string strStatus)
+
+
+        public ActionResult Index()
         {
             //Logger.Info("11111nlog test error");
             //Logger.Fatal("1111222222nlog test error");
             //Logger.Error("22134444nlog test error");
-            
+
             //LogEventInfo lei = new LogEventInfo();
             //lei.Properties["user_id"] = "18";
             //lei.Properties["user_name"] = "AllenLee";
@@ -26,37 +28,26 @@ namespace Beyond.Core.Admin.Controllers
             //lei.Properties["remark"] = "This is a remark";
             //lei.Level = LogLevel.Info;
             //Logger.Log(lei);
+            //var strStatus = "";
+            //if (string.IsNullOrEmpty(strStatus))
+            //{
+            //    return View("Login");
+            //}
 
-            if (string.IsNullOrEmpty(strStatus))
-            {
-                return View("Login");
-            }
+            //string userName = Request.Params["user_name"].ToString();//账号
+            //string password = Request.Params["password"].ToString(); //密码
+            //if (userName == "admin" && password == "123")
+            //{
+            //    return View("Index", "login");
+            //}
 
-            string userName = Request.Params["user_name"].ToString();//账号
-            string password = Request.Params["password"].ToString(); //密码
-            if (userName == "admin" && password == "123")
-            {
-                return View("Index", "login");
-            }
+            //return Content("The name or password is not correct");
 
-            return Content("The name or password is not correct");
+            return View();
         }
 
         public ActionResult Login()
         {
-            //测试数据 888
-            //这里是来自GitHubTest的分支修改测试
-            string strGitHub = "";
-            if (strGitHub == "")
-            {
-                strGitHub = "OK";
-            }
-            string strGitHub22 = "";
-            if (strGitHub22 == "")
-            {
-                strGitHub22 = "OK";
-            }
-
             return View();
         }
     }
